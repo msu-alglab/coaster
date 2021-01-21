a2406eb542a3c60a0376407706a0e6b96a5ff9b4
Lucy Williams <lgw2@uw.edu>
2021-01-21 11:35:52 -0700

    Add comment

--- cyclic-flows.py
+++ cyclic-flows.py
@@ -114,6 +114,7 @@ if __name__ == "__main__":
     """
         Main script
     """
+    # parse arguments
     readme_filename = path.join(path.dirname(__file__), 'readme_short.txt')
     with open(readme_filename, 'r') as desc:
         description = desc.read()
